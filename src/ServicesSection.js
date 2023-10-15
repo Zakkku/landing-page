4f23a7fc3c4c171755990ee89ea153aafc700f35
Zach <wakuwakuzakku@gmail.com>
2023-10-15 18:02:40 +0900

    our services section

--- src/ServicesSection.js
+++ src/ServicesSection.js
@@ -2,8 +2,16 @@ export default function ServiceSection() {
     return (
         <div className="services-section">
             <h2>
-                This is the Services Section!
+                Our Services
             </h2>
+
+            <div className="services-container">
+            <div className="service-block">
+                <img src="https://assets.codepen.io/6060109/agency-service-1.png" alt="service 1"></img>
+            </div>
+            <div className="service-block"></div>
+            <div className="service-block"></div>
+            </div>
         </div>
     );
 };
